f90bc82a6df2a7cac4f17ae21256a3eae19a4add
Hanslukas <gl.lamanna@gmail.com>
2022-05-01 21:37:15 +0200

    add hero image

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,14 @@ import { link } from './shared/linkNavbar';
   </Route>
   {#each link as item}
   <Route path={item.path}>
-    {item.title}  
+    <div class="w-full relative">
+      <div class="w-full h-full flex flex-col absolute top-0 left-0 justify-center items-center">
+        <div class="text-6xl font-bold text-white">{item.title}</div>
+        <div class="text-2xl text-white">Sottotitolo hhfdsfkjkfjksjfklsjfks</div>
+      </div>
+      <img src="src\assets\images\hero-image.jpg" alt="Image alt text" />
+    </div> 
+
   </Route>
   {/each}
 <Footer/>
