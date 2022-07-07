1e66312e8324f8906e7ee25ac2288e3cf6afacf2
Gianluca La Manna <gl.lamanna@gmail.com>
2022-07-07 17:41:46 +0200

    remove comment

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -8,7 +8,6 @@ import { link, linkSocial } from "../shared/linkNavbar";
 <nav class="flex items-center justify-between flex-wrap bg-purple-700 p-6 bg-gradient-to-r from-indigo-500 ">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
   
-    <!-- <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
     <Link href="/"><span class="font-semibold text-xl tracking-tight">Gianluca La Manna</span></Link>
   </div>
   <div class="block lg:hidden">
