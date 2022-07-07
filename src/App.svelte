2effae98cb02df4968185261d85cb34d88caf28b
Gianluca La Manna <gl.lamanna@gmail.com>
2022-07-07 17:42:31 +0200

    update app style

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
   import {Router, Route} from 'svelte-routing'
+import Avatar from './lib/Avatar.svelte';
+import Content from './lib/Content.svelte';
 import Footer from './lib/Footer.svelte';
+import Hero from './lib/Hero.svelte';
 import Navbar from './lib/Navbar.svelte';
 import { link } from './shared/linkNavbar';
 
@@ -9,24 +12,19 @@ import { link } from './shared/linkNavbar';
 <Router>
   <Navbar/>
   <Route path="/">
-    <div class="w-full relative">
+    <!-- <div class="w-full relative">
       <div class="w-full h-full flex flex-col absolute top-0 left-0 justify-center items-center">
         <div class="text-6xl font-bold text-white">Home</div>
         <div class="text-2xl text-white">Sottotitolo</div>
       </div>
       <img src="src\assets\images\hero-image.jpg" alt="Image alt text" />
-    </div> 
+    </div>  -->
+    <Hero/>
+
   </Route>
   {#each link as item}
   <Route path={item.path}>
-    <div class="w-full relative">
-      <div class="w-full h-full flex flex-col absolute top-0 left-0 justify-center items-center">
-        <div class="text-6xl font-bold text-white">{item.title}</div>
-        <div class="text-2xl text-white">Sottotitolo</div>
-      </div>
-      <img src="src\assets\images\hero-image.jpg" alt="Image alt text" />
-    </div> 
-
+    <Content {item}/>
   </Route>
   {/each}
 <Footer/>
