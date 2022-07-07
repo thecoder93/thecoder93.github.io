4585d3018e57bbff44969081f17f7559afc192a2
Gianluca La Manna <gl.lamanna@gmail.com>
2022-07-07 17:41:07 +0200

    change font roboto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 
   plugins: [],
