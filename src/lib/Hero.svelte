c42dcb9c067ee817f6c82dfe25548b83f63e30a0
Gianluca La Manna <gl.lamanna@gmail.com>
2022-07-07 17:42:46 +0200

    add hero component

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,15 @@
+<script>
+import Avatar from "./Avatar.svelte";
+
+</script>
+
+<div class="w-full relative">
+    <div class="w-full h-full flex flex-col absolute top-8 left-0 justify-center items-center">
+        <div class="text-7xl font-bold text-white mt-4">Gianluca La Manna</div>
+        <Avatar/>        
+      <div class="text-2xl text-white">Frontend Developer @ <a class="underline" href="https://www.flowing.it/">FLOWING</a></div>
+      <div class="text-md text-white mt-2"> from Sicily with ♥ </div>
+
+    </div>
+    <img src="src\assets\images\hero-image.jpg" alt="Image alt text" />
+  </div> 
